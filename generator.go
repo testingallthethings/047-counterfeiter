d2638ee5b0257a1057e0c81eb93e0c8c79f10230
Mark Bradley <braddle@gmail.com>
2022-07-04 14:44:27 +0100

    Refactoring tests to use mock generated by Counterfeiter

--- generator.go
+++ generator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+//go:generate  counterfeiter -o mock . Applicant
 type Applicant interface {
 	IsOver17() bool
 	HoldsLicence() bool
@@ -12,10 +13,12 @@ type Applicant interface {
 	GetDOB() string
 }
 
+//go:generate  counterfeiter -o mock . Logger
 type Logger interface {
 	LogStuff(v string)
 }
 
+//go:generate  counterfeiter -o mock . RandomNumbersGenerator
 type RandomNumbersGenerator interface {
 	GetRandomNumbers(len int) string
 }
@@ -26,21 +29,21 @@ type NumberGenerator struct {
 }
 
 func (g NumberGenerator) Generate(a Applicant) (string, error) {
-	if (a.HoldsLicence()) {
+	if a.HoldsLicence() {
 		g.l.LogStuff("Duplicate Applicant, you can only hold one licence")
 		return "", errors.New("Duplicate Applicant, you can only hold one licence")
 	}
 
-	if (!a.IsOver17()) {
-		g.l.LogStuff("Underaged Applicant, you must be 17 for a licence")
-		return "", errors.New("Underaged Applicant, you must be 17 for a licence")
+	if !a.IsOver17() {
+		g.l.LogStuff("Underaged Applicant, you must be 17 for applicant licence")
+		return "", errors.New("Underaged Applicant, you must be 17 for applicant licence")
 	}
 
 	n := fmt.Sprintf(
 		"%s%s",
 		a.GetInitials(),
 		a.GetDOB(),
-		)
+	)
 
 	num := 16 - len(n)
 
@@ -48,5 +51,5 @@ func (g NumberGenerator) Generate(a Applicant) (string, error) {
 }
 
 func NewNumberGenerator(l Logger, r RandomNumbersGenerator) NumberGenerator {
-	return NumberGenerator{l ,r}
+	return NumberGenerator{l, r}
 }
--- generator_test.go
+++ generator_test.go
@@ -2,66 +2,18 @@ package drivinglicence_test
 
 import (
 	"drivinglicence"
+	"drivinglicence/mock"
 	"github.com/stretchr/testify/suite"
-	"strings"
 	"testing"
 )
 
-func (s *DrivingLicenceSuite) TestUnderageApplicant() {
- 	a := UnderageApplicant{}
- 	l := &SpyLogger{}
-	r := FakeRand{}
-
- 	lg := drivinglicence.NewNumberGenerator(l, r)
- 	_, err := lg.Generate(a)
-
- 	s.Error(err)
-	s.Contains(err.Error(), "Underaged")
-
- 	s.Equal(1, l.callCount)
- 	s.Contains(l.lastMessage, "Underaged")
-}
-
-func (s *DrivingLicenceSuite) TestNoSecondLicence() {
-	a := LicenceHolderApplicant{}
-	l := &SpyLogger{}
-	r := FakeRand{}
-
-	lg := drivinglicence.NewNumberGenerator(l, r)
-	_, err := lg.Generate(a)
-
-	s.Error(err)
-	s.Contains(err.Error(), "Duplicate")
-
-	s.Equal(1, l.callCount)
-	s.Contains(l.lastMessage, "Duplicate")
-}
-
-func (s *DrivingLicenceSuite) TestLicenceGeneration() {
-	a := ValidApplicant{"MDB", "23082011"}
-	l := &SpyLogger{}
-	r := FakeRand{}
-
-	lg := drivinglicence.NewNumberGenerator(l, r)
-	ln, err := lg.Generate(a)
-
-	s.NoError(err)
-	s.Equal("MDB2308201100000", ln)
-}
-
-func (s *DrivingLicenceSuite) TestLicenceGenerationShorterInitials() {
-	a := ValidApplicant{"MB", "23082011"}
-	l := &SpyLogger{}
-	r := FakeRand{}
-
-	lg := drivinglicence.NewNumberGenerator(l, r)
-	ln, err := lg.Generate(a)
-
-	s.NoError(err)
-	s.Equal("MB23082011000000", ln)
-}
-
 type DrivingLicenceSuite struct {
+	applicant              *mock.FakeApplicant
+	logger                 *mock.FakeLogger
+	randomNumbersGenerator *mock.FakeRandomNumbersGenerator
+
+	lg drivinglicence.NumberGenerator
+
 	suite.Suite
 }
 
@@ -69,78 +21,63 @@ func TestDrivingLicenceSuite(t *testing.T) {
 	suite.Run(t, new(DrivingLicenceSuite))
 }
 
-type UnderageApplicant struct {}
+func (s *DrivingLicenceSuite) SetupTest() {
+	s.applicant = &mock.FakeApplicant{}
+	s.logger = &mock.FakeLogger{}
+	s.randomNumbersGenerator = &mock.FakeRandomNumbersGenerator{}
 
-func (u UnderageApplicant) GetDOB() string {
-	return ""
+	s.lg = drivinglicence.NewNumberGenerator(s.logger, s.randomNumbersGenerator)
 }
 
-func (u UnderageApplicant) GetInitials() string {
-	return ""
+func (s *DrivingLicenceSuite) TestUnderageApplicant() {
+	s.applicant.HoldsLicenceReturns(false)
+	s.applicant.IsOver17Returns(false)
+
+	_, err := s.lg.Generate(s.applicant)
+
+	s.Error(err)
+	s.Contains(err.Error(), "Underaged")
+
+	s.Equal(1, s.logger.LogStuffCallCount())
+	s.Contains(s.logger.LogStuffArgsForCall(0), "Underaged")
 }
 
-func (u UnderageApplicant) IsOver17() bool {
-	return false
+func (s *DrivingLicenceSuite) TestNoSecondLicence() {
+	s.applicant.HoldsLicenceReturns(true)
+
+	_, err := s.lg.Generate(s.applicant)
+
+	s.Error(err)
+	s.Contains(err.Error(), "Duplicate")
+
+	s.Equal(1, s.logger.LogStuffCallCount())
+	s.Contains(s.logger.LogStuffArgsForCall(0), "Duplicate")
 }
 
-func (u UnderageApplicant) HoldsLicence() bool {
-	return false
+func (s *DrivingLicenceSuite) TestLicenceGeneration() {
+	s.applicant.HoldsLicenceReturns(false)
+	s.applicant.IsOver17Returns(true)
+	s.applicant.GetInitialsReturns("MDB")
+	s.applicant.GetDOBReturns("23082011")
+
+	s.randomNumbersGenerator.GetRandomNumbersReturns("00000")
+
+	ln, err := s.lg.Generate(s.applicant)
+
+	s.NoError(err)
+	s.Equal("MDB2308201100000", ln)
 }
 
-type LicenceHolderApplicant struct {}
+func (s *DrivingLicenceSuite) TestLicenceGenerationShorterInitials() {
+	s.applicant.HoldsLicenceReturns(false)
+	s.applicant.IsOver17Returns(true)
+	s.applicant.GetInitialsReturns("MB")
+	s.applicant.GetDOBReturns("23082011")
 
-func (l LicenceHolderApplicant) IsOver17() bool {
-	return true
+	s.randomNumbersGenerator.GetRandomNumbersReturns("000000")
+
+	ln, err := s.lg.Generate(s.applicant)
+
+	s.NoError(err)
+	s.Equal("MB23082011000000", ln)
 }
-
-func (l LicenceHolderApplicant) HoldsLicence() bool {
-	return true
-}
-
-func (l LicenceHolderApplicant) GetInitials() string {
-	return ""
-}
-
-func (l LicenceHolderApplicant) GetDOB() string {
-	return ""
-}
-
-type ValidApplicant struct {
-	initials string
-	dob string
-}
-
-func (v ValidApplicant) IsOver17() bool {
-	return true
-}
-
-func (v ValidApplicant) HoldsLicence() bool {
-	return false
-}
-
-func (v ValidApplicant) GetInitials() string {
-	return v.initials
-}
-
-func (v ValidApplicant) GetDOB() string {
-	return v.dob
-}
-
-type FakeRand struct {}
-
-func (f FakeRand) GetRandomNumbers(len int) string {
-	return strings.Repeat("0", len)
-}
-
-type SpyLogger struct {
-	callCount int
-	lastMessage string
-}
-
-func (s *SpyLogger) LogStuff(v string) {
-	s.callCount++
-	s.lastMessage = v
-}
-
-
-
